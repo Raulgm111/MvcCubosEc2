cdb85372be0f52781124c4928dc12b268ea780a4
Raulgm111~ <raul.garciamoratinos@outlook.com~>
2023-05-30 13:12:06 +0200

    Add project files.

---
+++ MvcCubosEc2/Controllers/CubosController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCubosEc2.Models;
+using MvcCubosEc2.Repositories;
+
+namespace MvcCubosEc2.Controllers
+{
+    public class CubosController : Controller
+    {
+        private RepositoryCubos repo;
+
+        public CubosController(RepositoryCubos repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> GetCubos()
+        {
+            List<Cubo> cubos = await this.repo.GetCubosAsync();
+            return View(cubos);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Cubo>> FindCubo(int id)
+        {
+            Cubo cubo = await this.repo.FindCuboAsync(id);
+            return View(cubo);
+        }
+
+        public IActionResult CreateCubo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCubo(Cubo cubo)
+        {
+            await this.repo.CreateCuboAsync(cubo.Nombre,cubo.Marca,cubo.Imagen,cubo.Precio);
+            return RedirectToAction("GetCubos");
+        }
+
+        public async Task<ActionResult<Cubo>> UpdateCubo(int id)
+        {
+            Cubo cubo = await this.repo.FindCuboAsync(id);
+            return View(cubo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateCubo(Cubo cubo)
+        {
+            await this.repo.UpdateCuboAsync(cubo.IdCubo,cubo.Nombre,cubo.Marca,cubo.Imagen,cubo.Precio);
+            return RedirectToAction("GetCubos");
+        }
+
+        public async Task<ActionResult> DeleteCubo(int id)
+        {
+            await this.repo.DeleteCuboAsync(id);
+            return RedirectToAction("GetCubos");
+        }
+    }
+}
---
+++ MvcCubosEc2/Repositories/RepositoryCubos.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCubosEc2.Data;
+using MvcCubosEc2.Models;
+
+namespace MvcCubosEc2.Repositories
+{
+    public class RepositoryCubos
+    {
+
+        private CubosContext context;
+
+        public RepositoryCubos(CubosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Cubo>> GetCubosAsync()
+        {
+            return await this.context.Cubos.ToListAsync();
+        }
+
+        public async Task<Cubo> FindCuboAsync(int id)
+        {
+            return await
+                this.context.Cubos
+                .FirstOrDefaultAsync(x => x.IdCubo == id);
+        }
+
+        private int GetMaxIdCubo()
+        {
+            return this.context.Cubos.Max(x => x.IdCubo) + 1;
+        }
+
+        public async Task CreateCuboAsync(string nombre, string marca,string imagen, int precio)
+        {
+            Cubo cubo = new Cubo();
+            cubo.IdCubo = this.GetMaxIdCubo();
+            cubo.Nombre = nombre;
+            cubo.Marca = marca;
+            cubo.Imagen = imagen;
+            cubo.Precio = precio;
+            this.context.Cubos.Add(cubo);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCuboAsync(int id, string nombre, string marca, string imagen, int precio)
+        {
+            Cubo cubo = await this.context.Cubos.FindAsync(id);
+
+            if (cubo != null)
+            {
+                cubo.Nombre = nombre;
+                cubo.Marca = marca;
+                cubo.Imagen = imagen;
+                cubo.Precio = precio;
+
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteCuboAsync(int id)
+        {
+            Cubo cubo = await this.context.Cubos.FindAsync(id);
+
+            if (cubo != null)
+            {
+                this.context.Cubos.Remove(cubo);
+                await this.context.SaveChangesAsync();
+            }
+        }
+    }
+}
